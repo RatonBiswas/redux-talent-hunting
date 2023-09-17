29cbbf3e7403f64ca8b5ac5a9a8df060cafbd626
Raton Biswas <raton35-2241@diu.edu.bd>
2023-09-18 00:37:15 +0600

    use slicer setup

--- talent-hunting/src/main.jsx
+++ talent-hunting/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'normalize.css'
-import './index.css'
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "normalize.css";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App tab="home" />
+    </Provider>
   </React.StrictMode>,
-)
+);
