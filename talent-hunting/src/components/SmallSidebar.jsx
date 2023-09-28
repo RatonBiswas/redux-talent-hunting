79d395ac652773cceb7b5262f58d2e6eab4e1f90
Raton Biswas <raton35-2241@diu.edu.bd>
2023-09-29 01:20:17 +0600

    links data

--- talent-hunting/src/components/SmallSidebar.jsx
+++ talent-hunting/src/components/SmallSidebar.jsx
@@ -1,10 +1,37 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from 'react-router-dom';
+import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { toggleSidebar } from "../features/user/userSlice";
 const SmallSidebar = () => {
-    return (
-        <Wrapper>
-            SmallSidebar
-        </Wrapper>
-    );
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">Nav Links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
