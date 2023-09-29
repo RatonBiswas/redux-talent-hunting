0afeba2135c293a0716a50afd77415c9c420d1df
Raton Biswas <raton35-2241@diu.edu.bd>
2023-09-29 11:30:47 +0600

    big sidebar setup

--- talent-hunting/src/components/BigSidebar.jsx
+++ talent-hunting/src/components/BigSidebar.jsx
@@ -1,8 +1,23 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import NavLinks from "./NavLinks";
+import { useSelector } from "react-redux";
+import Logo from "./Logo";
+// import {AiOutlinePushpin} from 'react-icons'
+
 const BigSidebar = () => {
+    const {isSidebarOpen} = useSelector((store)=>store.user)
     return (
         <Wrapper>
-            BigSidebar
+            <div className={isSidebarOpen ? 'sidebar-container show-sidebar': 'sidebar-container'}>
+            {/* <div className= "sidebar-container show-sidebar"> */}
+                <div className="context">
+                    <header>
+                        <Logo/>
+                    </header>
+                    {/* <AiOutlinePushpin/> */}
+                    <NavLinks/>
+                </div>
+            </div>
         </Wrapper>
     );
 };
